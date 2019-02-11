0112e8efae442d3becec4167c817bb87afb3fd4a
Jack Connolly <jconnolly226@gmail.com>
2019-02-11 13:32:24 -0500

    5.6 complete

--- toDoList/DetailViewController.swift
+++ toDoList/DetailViewController.swift
@@ -12,13 +12,18 @@ class DetailViewController: UIViewController {
 
     @IBOutlet weak var toDoField: UITextField!
     @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    @IBOutlet weak var toDoNoteView: UITextView!
     var toDoItem: String?
+    var toDoNoteItem: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let toDoItem = toDoItem{
             toDoField.text = toDoItem
+        }
+        if let toDoNoteItem = toDoItem{
+            toDoNoteView.text = toDoNoteItem
         }
         enableDisableSaveButton()
         toDoField.becomeFirstResponder()
@@ -27,7 +32,9 @@ class DetailViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
+            toDoNoteItem = toDoNoteView.text
         }
+        
     }
     
     func enableDisableSaveButton() {
--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     var toDoArray = ["Learn Swift", "Build Apps", "Change the World"]
+    var toDoNotesArray = ["I should be certain to do all of the exercises at the end of the chapters before the exam",
+                          "Take my ideas to the school's venture competition", "Focus apps on empowerment for all"]
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -25,6 +27,8 @@ class ViewController: UIViewController {
             let destination = segue.destination as! DetailViewController
             let index = tableView.indexPathForSelectedRow!.row
             destination.toDoItem = toDoArray[index]
+            destination.toDoNoteItem = toDoNotesArray[index]
+            
         }else{
             if let selectedPath = tableView.indexPathForSelectedRow{
                 tableView.deselectRow(at: selectedPath, animated: false)
@@ -36,10 +40,12 @@ class ViewController: UIViewController {
         let sourceViewController = segue.source as! DetailViewController
         if let indexPath = tableView.indexPathForSelectedRow{
             toDoArray[indexPath.row] = sourceViewController.toDoItem!
+            toDoNotesArray[indexPath.row] = sourceViewController.toDoNoteItem!
             tableView.reloadRows(at: [indexPath], with: .automatic)
         }else {
             let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
             toDoArray.append(sourceViewController.toDoItem!)
+            toDoNotesArray.append(sourceViewController.toDoNoteItem!)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
         
@@ -71,20 +77,26 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotesArray[indexPath.row]
+        
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             toDoArray.remove(at: indexPath.row)
+            toDoNotesArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let itemToMove = toDoArray[sourceIndexPath.row]
+        let noteToMNove = toDoNotesArray[sourceIndexPath.row]
         toDoArray.remove(at: sourceIndexPath.row)
+        toDoNotesArray.remove(at: sourceIndexPath.row)
         toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        toDoNotesArray.insert(noteToMNove, at: destinationIndexPath.row)
     }
 }
 
