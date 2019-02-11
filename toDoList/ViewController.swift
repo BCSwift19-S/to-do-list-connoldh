eeeb804edf73791c3e20864e18ce4b998a12647d
Jack Connolly <jconnolly226@gmail.com>
2019-02-11 13:56:34 -0500

    5.7 complete

--- toDoList/ViewController.swift
+++ toDoList/ViewController.swift
@@ -13,13 +13,28 @@ class ViewController: UIViewController {
     @IBOutlet weak var editBarButton: UIBarButtonItem!
     @IBOutlet weak var addBarButton: UIBarButtonItem!
     
-    var toDoArray = ["Learn Swift", "Build Apps", "Change the World"]
-    var toDoNotesArray = ["I should be certain to do all of the exercises at the end of the chapters before the exam",
-                          "Take my ideas to the school's venture competition", "Focus apps on empowerment for all"]
+    var toDoArray = [String]()
+    var toDoNotesArray = [String]()
+    
+    
+//    var toDoArray = ["Learn Swift", "Build Apps", "Change the World"]
+//    var toDoNotesArray = ["I should be certain to do all of the exercises at the end of the chapters before the exam",
+//                          "Take my ideas to the school's venture competition", "Focus apps on empowerment for all"]
+    var defaultsData = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
+        toDoArray = defaultsData.stringArray(forKey: "toDoArray") ?? [String]()
+        toDoNotesArray = defaultsData.stringArray(forKey: "toDoNotesArray") ?? [String]()
+        
+    }
+    
+    func saveDefaultsData(){
+        defaultsData.set(toDoArray, forKey: "toDoArray")
+        defaultsData.set(toDoNotesArray, forKey: "toDoNotesArray")
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -48,6 +63,8 @@ class ViewController: UIViewController {
             toDoNotesArray.append(sourceViewController.toDoNoteItem!)
             tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
+        
+        saveDefaultsData()
         
         
     }
@@ -87,6 +104,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
             toDoArray.remove(at: indexPath.row)
             toDoNotesArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            saveDefaultsData()
         }
     }
     
@@ -97,6 +115,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource{
         toDoNotesArray.remove(at: sourceIndexPath.row)
         toDoArray.insert(itemToMove, at: destinationIndexPath.row)
         toDoNotesArray.insert(noteToMNove, at: destinationIndexPath.row)
+        saveDefaultsData()
     }
 }
 
